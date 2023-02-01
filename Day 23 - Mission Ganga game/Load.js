c0a5567527bb4bd929fd2727abe85e56d4756bba
knight070409 <yashp040907@gmail.com>
2023-02-01 21:06:24 +0530

    Added Game start scene and physics on player

---
+++ Day 23 - Mission Ganga game/Load.js
@@ -0,0 +1,31 @@
+class Load extends Phaser.Scene {
+    constructor(){
+        super("loadScreen")
+    }
+
+    preload(){
+
+        this.load.image("river", "assest/river.png");
+        this.load.image("background", "assest/background.png");
+        this.load.image("bottle1", "assest/bottle1.png");
+        this.load.image("bottle2", "assest/bottle2.png");
+        this.load.image("can", "assest/can.png");
+        this.load.image("washer", "assest/washer.png");
+        this.load.image("player", "assest/player.png");
+        this.load.image("Startscene", "assest/scene.png");
+    }
+
+    create(){
+        this.startscene = this.add.sprite(0, 0, "Startscene");
+        this.startscene.setScale(1, 0.76)
+        this.startscene.setOrigin(0,0);
+
+        this.ENTERKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+    }
+
+    update(){
+        if(this.ENTERKey.isDown){
+            this.scene.switch("bootGame");
+        }
+    }
+}
---
+++ Day 23 - Mission Ganga game/Scene1.js
@@ -0,0 +1,85 @@
+class Scene1 extends Phaser.Scene {
+    constructor(){
+        super("bootGame")
+    }
+
+    create(){
+        this.river = this.add.tileSprite(0, 0,config.width, config.height, "river");
+        this.river.setOrigin(0, 0);
+
+        this.background = this.add.tileSprite(0,0,config.width, 342, "background");
+        this.background.setOrigin(0,0);
+        this.background.setScale(1, 0.3);
+
+        /*this.bottle1 = this.add.sprite(config.width - 50, config.height / 2 +135, "bottle1");
+        this.bottle1.setScale(0.1);
+
+        this.bottle2 = this.add.sprite(config.width - 50, config.height / 2 -100, "bottle2");
+        this.bottle2.setScale(0.125);
+
+        this.can = this.add.sprite(config.width - 50, config.height / 2 -128, "can");
+        this.can.setScale(0.1);
+
+        this.washer = this.add.sprite(config.width -50, config.height / 2 +100, "washer");
+        this.washer.setScale(0.15);
+
+        this.bottle1.setInteractive();
+        this.bottle2.setInteractive();
+        this.washer.setInteractive();
+        this.can.setInteractive();*/
+
+        this.physics.world.setBoundsCollision();
+
+        this.player = this.physics.add.image(0, config.height/2, "player");
+        this.player.setScale(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.WKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.AKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.SKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.DKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+    }
+
+    update(){
+        this.river.tilePositionX += 1;
+        this.background.tilePositionX += 1;
+
+        //this.moveGarbage(this.bottle1, 2);
+        //this.moveGarbage(this.bottle2, 2);
+        //this.moveGarbage(this.can, 3);
+        //this.moveGarbage(this.washer, 3);
+
+        this.movePlayerManager();
+    }
+
+    moveGarbage(garbage, speed){
+        garbage.x -= speed;
+        if(garbage.x < 0){
+            this.ResetPos(garbage);
+        }
+    }
+
+    ResetPos(garbage) {
+        garbage.x = config.width;
+        var randomY = Phaser.Math.Between(config.height / 2 - 128, config.height);
+        garbage.y = randomY;
+    }
+
+    movePlayerManager() {
+        this.player.setVelocity(0);
+    
+        if (this.AKey.isDown) {
+          this.player.setVelocityX(-gameSettings.playerSpeed);
+        } else if (this.DKey.isDown) {
+          this.player.setVelocityX(gameSettings.playerSpeed);
+        }
+    
+        if (this.WKey.isDown) {
+          this.player.setVelocityY(-gameSettings.playerSpeed);
+        } else if (this.SKey.isDown) {
+          this.player.setVelocityY(gameSettings.playerSpeed);
+        }
+    }
+}
+
